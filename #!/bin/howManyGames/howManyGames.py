a300b5c310f3590976c2b74b64439ee22609fee1
bberrakela <tekinelaberrak@gmail.com>
2025-08-12 12:17:58 +0300

    Create howManyGames.py

---
+++ #!/bin/howManyGames/howManyGames.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'howManyGames' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER p
+#  2. INTEGER d
+#  3. INTEGER m
+#  4. INTEGER s
+#
+
+def howManyGames(p, d, m, s):
+    game_count = 0
+    current_price = p
+    current_budget = s
+    
+    while current_budget >= current_price:
+        current_budget -= current_price
+        game_count += 1
+        current_price = max(current_price - d, m)
+        
+    return game_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    p = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    m = int(first_multiple_input[2])
+
+    s = int(first_multiple_input[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
